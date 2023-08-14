782875fa954f3c59e5cf58012c4eb876d1e755f2
tima1704 <ibatullintima@gmail.com>
2023-08-14 10:38:11 +0600

    add: auth

--- src/pages/Auth/SignIn/index.tsx
+++ src/pages/Auth/SignIn/index.tsx
@@ -1,37 +1,38 @@
-import React from "react"
-import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline"
-import { useForm } from "react-hook-form"
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { FORMERRORMESAGE, FORMREGEX } from "../../../constants/common";
+import { AuthService } from "../../../helpers/api/authorization";
+
+interface IForm {
+  username: string;
+  password: string;
+}
 
 export const SignIn = () => {
-  const [type, setType] = React.useState("text")
-
   const {
     handleSubmit,
-    reset,
     register,
+    reset,
     formState: { errors },
-  } = useForm();
+  } = useForm<IForm>();
 
-  const passwordInputRef = React.useRef<HTMLInputElement>(null);
-
-  const onToggle = () => {
-    setType((current) => (current === "text" ? "password" : "text"));
-
-    passwordInputRef.current?.focus();
-    const passwordLength = passwordInputRef.current?.value.length ?? null;
-    setTimeout(() => {
-      passwordInputRef.current?.setSelectionRange(
-        passwordLength,
-        passwordLength
+  const onSubmit: SubmitHandler<IForm> = async (data) => {
+    try {
+      await AuthService.getAuthUser(data).then((res) =>
+        localStorage.setItem("accessToken", res.data.access)
       );
-    }, 1);
+    } catch (error: any) {
+      throw new Error(error);
+    } finally {
+      reset();
+    }
   };
 
   return (
     <header>
       <div className="h-screen bg-gray-100 flex justify-center items-center">
         <div className="py-6 px-8 h-80 w-96 bg-white rounded shadow-xl">
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="mb-6">
               <label htmlFor="name" className="block text-gray-800 font-bold">
                 Name:
@@ -39,38 +40,28 @@ export const SignIn = () => {
               <input
                 type="text"
                 id="name"
-                {...register("name", {
-                  required: "Обязательное поле!",
+                {...register("username", {
+                  required: FORMERRORMESAGE.required,
                   maxLength: {
                     value: 15,
-                    message: "максимум 15 символов",
+                    message: FORMERRORMESAGE.username.maxLength,
                   },
                   minLength: {
-                    value: 1,
-                    message: "минимум 1 символ",
+                    value: 3,
+                    message: FORMERRORMESAGE.username.minLength,
+                  },
+                  pattern: {
+                    value: FORMREGEX.username,
+                    message: FORMERRORMESAGE.username.pattern,
                   },
                 })}
                 placeholder="Username"
                 className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600"
               />
+              {errors.username && (
+                <p className="text-red-600">{errors.username?.message}</p>
+              )}
             </div>
-
-            <div className="mb-6">
-              <label htmlFor="name" className="block text-gray-800 font-bold">
-                Email:
-              </label>
-              <input
-                type="email"
-                id="email"
-                {...register("email", {
-                  required: "Обязательное поле!",
-                  pattern: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/,
-                })}
-                placeholder="Username"
-                className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600"
-              />
-            </div>
-
             <div>
               <label
                 htmlFor="password"
@@ -80,34 +71,25 @@ export const SignIn = () => {
               </label>
               <label htmlFor="password" className="relative">
                 <input
-                  // ref={passwordInputRef}
-                  type={type}
+                  type="password"
                   {...register("password", {
-                    required: "Обязательное поле",
+                    required: FORMERRORMESAGE.required,
                     maxLength: {
                       value: 40,
-                      message: "максимум 40 символов",
+                      message: FORMERRORMESAGE.password.maxLength,
                     },
                     minLength: {
                       value: 5,
-                      message: "минимум 5 символов",
+                      message: FORMERRORMESAGE.password.minLength,
                     },
                   })}
                   id="password"
                   placeholder="Password"
                   className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600 pr-8"
                 />
-                <button
-                  type="button"
-                  className="absolute right-2 z-0 w-[20px] h-[20px] top-1/2 -translate-y-1/2 border-none outline-none"
-                  onClick={onToggle}
-                >
-                  {type ? (
-                    <EyeIcon className="text-indigo-600" />
-                  ) : (
-                    <EyeSlashIcon className="text-indigo-600" />
-                  )}
-                </button>
+                {errors.password && (
+                  <p className="text-red-600">{errors.password?.message}</p>
+                )}
               </label>
             </div>
             <button className="cursor-pointer py-2 px-4 block mt-6 bg-indigo-500 text-white font-bold w-full text-center rounded">
