398d8e27b67836e6f64541ea7ed1947541525e5e
tima1704 <ibatullintima@gmail.com>
2023-08-15 10:59:15 +0600

    fix: auth route, sidebar buttons

--- src/pages/Cabinet/index.tsx
+++ src/pages/Cabinet/index.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { AppWrapper } from "../../components/main/AppWrapper";
-import { Navigate } from "react-router-dom";
-import { ROUTES } from "../../constants/routes";
+import { NoAccess } from "../../components/common/NoAccess";
 
 export const Cabinet = () => {
   const user = localStorage.getItem('accessToken')
-  
-  if(!user) return <Navigate to={ROUTES.auth.authRoute} />
+
+  if(!user) return <NoAccess />
   
   return (
     <React.Fragment>
