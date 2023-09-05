abf9a45987b2b4fcb1d83be340852dca90d1b6ac
Experadog <lsstudent008@mail.ru>
2023-09-05 18:07:28 +0600

    dev-vlad

--- src/components/main/SideBar/index.tsx
+++ src/components/main/SideBar/index.tsx
@@ -7,24 +7,35 @@ import { ROUTES } from "../../../constants/routes";
 interface ILinks {
   name: string;
   modal: TModals;
+  image: string;
 }
 
+<img src="/src/components/common/icon/" alt="" />;
 const sidebarModalText: ILinks[] = [
   {
     name: "Переключить страницу",
     modal: "switchPages",
+    image: "/src/components/common/icon/pages.svg",
   },
   {
     name: "Добавить блок",
     modal: "addBlocks",
+    image: "/src/components/common/icon/add_block.svg",
+  },
+  {
+    name: "Добавить пост",
+    modal: "addBlocks",
+    image: "/src/components/common/icon/add_post.svg",
   },
   {
     name: "Изменить контент",
     modal: "modifyContent",
+    image: "/src/components/common/icon/add_post.svg",
   },
   {
     name: "Загрузить фото",
     modal: "uploadImages",
+    image: "/src/components/common/icon/gallery.svg",
   },
 ];
 
@@ -39,27 +50,33 @@ const Sidebar: React.FC = () => {
     navigate(ROUTES.auth.authRoute);
   };
 
-  const { pathname } = useLocation()
+  const { pathname } = useLocation();
 
   return (
     <aside
       id="logo-sidebar"
-      className="fixed top-0 left-0 z-10 w-1/6 h-screen transition-transform -translate-x-full sm:translate-x-0 bg-gray-50 dark:bg-gray-800"
+      className="fixed top-0 left-0 z-10 w-[19%] h-screen transition-transform -translate-x-full sm:translate-x-0 bg-[rgba(255,255,255,1)] dark:bg-[rgba(255,255,255,1)]"
       aria-label="Sidebar"
     >
       <div className="h-full flex flex-col justify-between px-3 py-4 overflow-y-auto">
         <div>
-          <p className="flex items-center pl-2.5 mt-5 text-white text-xl">
-            Salymbekov admin
-          </p>
-          <p className="flex items-center pl-2.5 my-3 text-white">
-            Current page: {pathname}
-          </p>
-          <ul className="space-y-2 font-medium mt-10">
+          <p className="text-[#013CC6] text-[26px] font-[500]">Salymbekov admin</p>
+          <div className="flex items-center mt-2">
+            <img
+              className="w-5"
+              src="/src/components/common/icon/homik.svg"
+              alt="home"
+            />
+            <p className="flex items-center pl-2 my-3 text-[#CBCCCC]">
+              Current page: {pathname}
+            </p>
+          </div>
+          <ul className="space-y-8 font-medium mt-10">
             {sidebarModalText.map((item, index) => (
-              <li key={index}>
+              <li className="flex" key={index}>
+                <img className="w-4" src={item.image} alt={item.name} />
                 <p
-                  className="block px-4 py-2 cursor-pointer font-medium text-lg text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                  className="block whitespace-nowrap leading-[0.32px] hover:duration-500 px-2 py-2 cursor-pointer text-['Poppins'] font-[400] text-[16px] text-[#717171] dark:hover:text-[#0B63F8]"
                   onClick={() => {
                     setModalViewAction(item.modal);
                   }}
@@ -74,13 +91,13 @@ const Sidebar: React.FC = () => {
           <button
             onClick={handleExit}
             type="button"
-            className="text-white bg-[#0a0e0f] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-3 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 mr-2 mb-2"
+            className="text-white bg-[#013CC6] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#0B63F8]/50 font-medium rounded-lg text-sm px-2 py-2 text-center inline-flex items-center hover:duration-300 dark:focus:ring-[#0B63F8]/50 dark:hover:bg-[#0B63F8]/90 mr-2 mb-2"
           >
             выйти
           </button>
           <button
             type="button"
-            className="text-white bg-[#0a0e0f] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-3 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 mr-2 mb-2"
+            className="text-white bg-[#013CC6] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#0B63F8]/50 font-medium rounded-lg text-sm px-3 py-2.5 text-center inline-flex items-center hover:duration-300 dark:focus:ring-[#0B63F8]/50 dark:hover:bg-[#0B63F8]/90 mr-2 mb-2"
           >
             Вернуться на сайт
           </button>
