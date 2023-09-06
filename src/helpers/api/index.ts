5b33ade896f23db6af78c03536343cce6f4cb6eb
Anvar Baratov <superanvar11@gmail.com>
2023-09-06 15:42:40 +0600

    rest link changed

--- src/helpers/api/index.ts
+++ src/helpers/api/index.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios'
 
-export const BASE_API = 'http://13.127.216.121/api/v1/'
+export const BASE_API = 'http://3.6.88.179/api/v1/'
 
 export const $API: AxiosInstance = axios.create({
   baseURL: BASE_API
