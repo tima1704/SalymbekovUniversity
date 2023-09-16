ca4892368c45b22d1e535ef3c3d21e45c91d6208
Anvar Baratov <superanvar11@gmail.com>
2023-09-16 17:40:17 +0600

    partner list4

--- src/components/templates/Navbar/index.ts
+++
@@ -1,9 +0,0 @@
-import PartnerList1 from './PartnerList1';
-import PartnerList2 from './PartnerList2';
-import PartnerList3 from './PartnerList3';
-
-export default [
-  PartnerList1,
-  PartnerList2,
-  PartnerList3,
-]
